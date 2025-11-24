7e55d2277ba3c214ba79ad75c0653fe30412801b
WellWellGood <wellwellgood@example.com>
2025-11-24 17:42:35 +0900

    ReactProject

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: "/", // 커스텀 도메인 루트(kimkiyun.com)에 붙일 때는 "/" 유지
 });
